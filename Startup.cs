c597fa5bc799ebcfee4fc5e847356044ae26e9bc
TanzeemAlam <Tanzeemalam789@gmail.com>
2020-10-14 14:58:57 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace azure_assignment
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World, Tanzeem!");
-                })
+                });
             });
         }
     }
